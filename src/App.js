27b92cdaa50d58bd4cb59ab0c5806615c6515165
unknown <salmanpatrick5@gmail.com>
2022-08-26 21:32:57 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -24,22 +24,26 @@ function App() {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
+                  <a class="nav-link active" aria-current="page" href="#header">
                     Home
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
+                  <a
+                    class="nav-link active"
+                    aria-current="page"
+                    href="#checkout"
+                  >
                     Features
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
+                  <a class="nav-link active" aria-current="page" href="#money">
                     Solutions
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
+                  <a class="nav-link active" aria-current="page" href="#about">
                     About Us
                   </a>
                 </li>
